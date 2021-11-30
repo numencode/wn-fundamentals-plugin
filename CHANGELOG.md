0187992c2687f98dd9f0214b29e8f1296895a7b1
Blaz Orazem <blaz@orazem.si>
2021-11-30 11:57:58 +0100

    Plugin restructure

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+All notable changes to `numencode/wn-fundamentals-plugin` will be documented in this file
+
+## 2.0.0 - 2021-11-30
+
+- migration to Winter CMS
+
+## 1.0.1 - 2021-02-04
+
+- plugin cleanup
+
+## 1.0.0 - 2020-05-19
+
+- initial release
--- Plugin.php
+++ Plugin.php
@@ -3,17 +3,9 @@
 use ReflectionProperty;
 use System\Classes\PluginBase;
 use Winter\Translate\Classes\EventRegistry;
-use NumenCode\Fundamentals\Console\DbPullCommand;
-use NumenCode\Fundamentals\Console\DbBackupCommand;
 use NumenCode\Fundamentals\Bootstrap\ConfigOverride;
-use NumenCode\Fundamentals\Console\MediaPullCommand;
 use NumenCode\Fundamentals\Extensions\TwigExtension;
 use NumenCode\Fundamentals\Bootstrap\BackendOverride;
-use NumenCode\Fundamentals\Console\MediaBackupCommand;
-use NumenCode\Fundamentals\Console\ProjectPullCommand;
-use NumenCode\Fundamentals\Console\ProjectBackupCommand;
-use NumenCode\Fundamentals\Console\ProjectCommitCommand;
-use NumenCode\Fundamentals\Console\ProjectDeployCommand;
 use NumenCode\Fundamentals\Bootstrap\OverrideFormWidgets;
 use NumenCode\Fundamentals\Extensions\EventRegistryExtension;
 
@@ -39,25 +31,12 @@ class Plugin extends PluginBase
     public function register()
     {
         $this->registerHelpers();
-        $this->registerConsoleCommands();
         $this->registerTranslatable();
     }
 
     protected function registerHelpers()
     {
         require_once __DIR__ . '/helpers.php';
-    }
-
-    protected function registerConsoleCommands()
-    {
-        $this->registerConsoleCommand('numencode.db_pull', DbPullCommand::class);
-        $this->registerConsoleCommand('numencode.db_backup', DbBackupCommand::class);
-        $this->registerConsoleCommand('numencode.media_pull', MediaPullCommand::class);
-        $this->registerConsoleCommand('numencode.media_backup', MediaBackupCommand::class);
-        $this->registerConsoleCommand('numencode.project_pull', ProjectPullCommand::class);
-        $this->registerConsoleCommand('numencode.project_backup', ProjectBackupCommand::class);
-        $this->registerConsoleCommand('numencode.project_commit', ProjectCommitCommand::class);
-        $this->registerConsoleCommand('numencode.project_deploy', ProjectDeployCommand::class);
     }
 
     protected function registerTranslatable()
